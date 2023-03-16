75d3edc2068fe99ea9ec3aaac2f071eeeee1a039
bshuler <BertShuler@proton.me>
2023-03-16 13:28:22 -0400

    1 file modified

--- src/components/SeedPhraseImport.vue
+++ src/components/SeedPhraseImport.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, reactive } from 'vue';
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
+const seedPhrase = ref('');
+
+const reverseSeedPhrase = computed(() => {
+  return seedPhrase.reverse();
+});
 </script>
 
 <template>
   <h2>{{ msg }}</h2>
   <div class="card">
     <p>Seed Phrase Import</p>
-    <p><input v-model="text"></p>
-    <p><button @click="$emit('decodeSeedPhrase')">Decode Seed Phrase</button></p>
+    <p>Seed Phrase: <input v-model="seedPhrase" /></p>
+    <p><button @click="decodeSeedPhrase">Decode Seed Phrase</button></p>
+    <p>Decoded Seed Phrase</p>
+    <p>{{ reverseSeedPhrase() }}</p>
   </div>
 </template>
 
