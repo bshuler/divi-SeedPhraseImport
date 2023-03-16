a045b65d350bb81acdd28a4fba62fe31e7f0e9ae
bshuler <BertShuler@proton.me>
2023-03-16 14:54:04 -0400

    3 files modified

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,31 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-
-  resolve: {
-    alias: {
-      // polyfills
-      Buffer: 'vite-compatible-readable-buffer',
-      stream: 'vite-compatible-readable-stream',
-      util: 'rollup-plugin-node-polyfills/polyfills/util',
+  base: '',
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()],
     },
   },
 });
